78630ca564a3546599e83166e737f46adf51f7d0
Djavani Gomes <drgomes5@stefanini.com>
2018-01-12 14:45:28 -0200

    Incluindo o Firebase SDK no projeto

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -3,6 +3,6 @@ import { Usuario } from './acesso/usuario.model';
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario): void {
         console.log('chegamos ate o servico', usuario);
-        
+
     }
 }
